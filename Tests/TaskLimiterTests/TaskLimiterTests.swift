d931be86fcba42e61c7e2fc7f045cf646533251a
Nikolai Ruhe <git@nikolai-ruhe.de>
2025-03-01 11:54:30 +0100

    Enhance unit test and prepare for extended testing.

--- Tests/TaskLimiterTests/TaskLimiterTests.swift
+++ Tests/TaskLimiterTests/TaskLimiterTests.swift
@@ -6,20 +6,25 @@ import os
 
 @Test func testBasicFunction() async throws {
     let limit = TaskLimiter(limitTo: 2)
+    let checker = TaskLimitChecker(expectedMaximumConcurrency: 2)
 
-    let count = OSAllocatedUnfairLock(initialState: 0)
-    try await withThrowingTaskGroup(of: Void.self) { group in
+    let results = try await withThrowingTaskGroup(of: Void.self) { group in
         for _ in 0..<10 {
             group.addTask {
                 try await limit {
-                    count.withLock { $0 += 1; #expect($0 <= 2) }
-                    defer { count.withLock { $0 -= 1 } }
-
-                    try await Task.sleep(for: .milliseconds(10))
+                    try await checker.expectLimitedConcurrency {
+                        try await Task.sleep(for: .milliseconds(10))
+                    }
                 }
             }
         }
 
         try await group.waitForAll()
+        return checker.state
     }
+
+    #expect(results.startedTaskCount == 10)
+    #expect(results.successfulTaskCount == 10)
+    #expect(results.failedTaskCount == 0)
+}
 }
